3acedc8559060e23785a1a3b293aedf6d40f1596
michaelmargolis <memargolis@gmail.com>
2023-04-10 18:30:17 +0100

    updated tags to avoid conflict with other services

--- asipLidar/ld06Lidar.h
+++ asipLidar/ld06Lidar.h
@@ -2,7 +2,7 @@
    code to read LdRobot LD06 lidar and send distance data over serial
    Tested on Pi Pico but should work on other 32 bit boards with free hardware serial port
 
-  All messages begin with '@R' and are terminated with newline 
+  All messages begin with '@N' and are terminated with newline 
 
    Note that range data is sent as millimeters and the rotation order of range data is clockwise
 
@@ -11,10 +11,11 @@
     range_header: "@Nr", 
     scan_id, (value must match value in descriptor message) 
     range_id, (must be one more than previous msg for this scan)  
-    ranges (mm) (comma seperated distances terminated by newline)
+    todo - update this with binary payload format
+    ranges (mm) (binary distances terminated by newline)
 
     Range terminator message:
-    header: "@Rt",
+    header: "@Nt",
     scan_number,  (value must match previous range msgs 
     angle_min, (angle*100)
     angle_max, (andgle*100)
@@ -26,6 +27,7 @@
    */
 
 #include <vector>
+#include <algorithm>
 
 enum {
   PKG_HEADER = 0x54,
@@ -35,8 +37,8 @@ enum {
   DISTANCE_HEADER = 'D',   // header for comma separated distance values
 };
 
-#define RANGE_HEADER "@Nr"  // distance data for part of one scan
-#define RANGE_TERM_HEADER "@Nt"  // message following range msgs for one complete scan
+#define RANGE_HEADER "%N,r"  // distance data for scan with binary payload
+#define RANGE_TERM_HEADER "@N,t"  // message following range msgs for one complete scan
 
 // the following two structs represent data sent from LD06
 typedef struct __attribute__((packed)) {
@@ -68,12 +70,16 @@ private:
   int32_t prev_LSA;
   HardwareSerial *lidarSerial; // connection to lidar
   Stream *stream;  // processed output data
+  int32_t fovLimit1; // angles must be less than this 
+  int32_t fovLimit2; // or greater than this 
  
 
 public:
-  int32_t scan_id;   // nbr of complete revolutions of the scanner
-  int32_t angleMin; // start angle of scan in degrees * 100
-  int32_t angleMax; // end angle of scan in degrees * 100
+  int32_t scan_id;    // nbr of complete revolutions of the scanner
+  int32_t angleMin;   // start angle of scan in degrees * 100
+  int32_t angleMax;   // end angle of scan in degrees * 100
+  int16_t FOV = 360;  // field of view in degrees
+
 
   int rangeMin = 0; // todo min and max are not yet populated
   int rangeMax = 0;
@@ -92,9 +98,27 @@ public:
     stream = outStream;
   }
 
+  void setFieldOfView(int16_t fovAngle) {
+     // angle in degrees
+     FOV = fovAngle;
+     fovLimit1 = FOV*50; // sensor is in degrees * 100 
+     fovLimit2 = 36000 - (FOV * 50);     
+  }      
+  
+  void update_range_data(int firstRangeIndex, int nbrRanges){
+    if( scan_id > 0 && nbrRanges > 0) { // ignore initial calibration scans
+      for (int i = firstRangeIndex; i < firstRangeIndex+nbrRanges; i++) {
+        ranges.push_back(LidarFrame.point[i].distance);              
+      }
+    }
+  }
+  
   void sendRangeMsg(int firstRangeIndex, int nbrRanges){
+    update_range_data(firstRangeIndex, nbrRanges);
+    return;
     if( scan_id > 0 && nbrRanges > 0) { // ignore initial calibration scans
       stream->printf("%s,%d,%d", RANGE_HEADER, scan_id, sequence_id);
+      
       for (int i = firstRangeIndex; i < firstRangeIndex+nbrRanges; i++) {
         stream->printf(",%d", LidarFrame.point[i].distance);
       }
@@ -104,15 +128,51 @@ public:
     }
   }
 
+  void processScanEnd() {
+      if( scan_id > 0) {
+        unsigned long startMicros = micros();
+        auto min = std::min_element(ranges.begin(), ranges.end());
+        rangeMin =  *min < 20 ? 20: *min;
+        auto max = std::max_element(ranges.begin(), ranges.end());
+        rangeMax = *max;   
+
+        /* TEMP for testing
+        ranges.clear();
+        ranges.push_back(1);    ranges.push_back(2);   ranges.push_back(3);   ranges.push_back(4);  ranges.push_back(5);  
+        */
+        
+        int16_t nbrBytes = ranges.size() * sizeof(uint16_t);
+        
+        stream->printf("%s,%d,%d,%d", RANGE_HEADER, scan_id, sequence_id, nbrBytes);
+        stream->write('\n');
+        stream->write(lowByte(nbrBytes));  stream->write(highByte(nbrBytes)); 
+        stream->write((char*)&ranges[0], nbrBytes);
+        stream->printf("\n"); // send newline to confirm end of binary message
+        
+        int nbrSequences = 1; // all range data sent in single binary packet
+        stream->printf("%s,%d,%d,%d,%d,%d,%d,%d\n", RANGE_TERM_HEADER, scan_id, angleMin, angleMax, rangeMin, rangeMax, nbrSequences, ranges.size());
+        
+        ranges.clear();   
+                
+        unsigned long dur = micros() - startMicros;    
+        Serial.printf("!Dur in micros = %d, min=%d, max=%d, bytes=%d\n", dur, rangeMin, rangeMax, nbrBytes  );        
+      }
+      scan_id++;
+  }
   void sendRangeScanEnd() {
+    processScanEnd();
+    return;
     if( scan_id > 0){ // ignore initial calibration scans
       stream->printf("%s,%d,%d,%d,%d,%d,%d\n", RANGE_TERM_HEADER, scan_id, angleMin, angleMax, rangeMin, rangeMax, rangeCount);
     }
     scan_id++;
     sequence_id = 0;
-    rangeCount = 0;
+    rangeCount = 0; 
   }
 
+  bool isWithinFOV(int angle){
+      return angle <= fovLimit1 || angle >= fovLimit2;
+  }
   void calculate() {
     // note angle values from lidar are hundredths of a degree
     float span = LidarFrame.LSA - LidarFrame.FSA; // difference between last and first angle
