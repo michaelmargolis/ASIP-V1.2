139ad900a75e26364556ccbf79ebe1b61b149169
michaelmargolis <memargolis@gmail.com>
2023-04-26 11:11:14 +0100

    added pins to begin method to enable reservation

--- asipLCD/examples/testLCD/testLCD.ino
+++ asipLCD/examples/testLCD/testLCD.ino
@@ -8,9 +8,20 @@
 #include <asip.h>       // the base class definitions
 #include <asipIO.h>     // the core I/O class definition
 
+    #define _U8G_LCD_ // use u8g2lib
+    #include <U8g2lib.h> 
+    U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+    //U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, SCL, SDA); // 128x32 oled
+    #define nbrLcdPins 2 // lcd pins to be reserved
+    pinArray_t lcdPins[nbrLcdPins] = {SCL, SDA}; // pins to reserve
+    #define LCD_FONT lcd_MEDIUM_FONT
+    #define MAX_LCD_LINES 5
+    #define MAX_LCD_CHARS_PER_LINE  21 
+    #define IS_LCD_FLIPPED false
+
 #include <asipLCD.h>
 
-char const *sketchName = "LCD Test";
+char const *sketchName = "LCD Test sketch";
 
 // create the services
 asipLCDClass asipLCD(id_LCD_SERVICE, NO_EVENT);
@@ -25,7 +36,7 @@ void setup()
 {
   beginAsipComms();
   // start the services   
-  asipLCD.begin();
+  asipLCD.begin(nbrLcdPins, lcdPins, LCD_FONT);
   asipIO.begin();  // NEW from  v1.1: core I/O service must follow all other service begin methods
 
   for (unsigned int i = 0; i < asipServiceCount(services); i++)
